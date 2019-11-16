b97787a151d948d1b14a6bd20687864181e0f738
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-11-16 18:23:32 +0200

    added simple bot

---
+++ bot/messages.py
@@ -0,0 +1,12 @@
+# The default message when the command /start is issued.
+START = 'Hi!'
+
+# The default message when the command /start is issued.
+STOP = 'See you soon!'
+
+# The default message when the command /help is issued.
+HELP = 'Help!'
+
+# The default message when bot receives invalid message, i.e.
+#   non-text/non-voice message.
+INVALID = 'Invalid message: please, send me text or voice question!'

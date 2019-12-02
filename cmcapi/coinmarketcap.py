405a2b92c4d5637cb9ea7eaaa097d33bb9304a32
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-12-02 07:34:50 +0200

    added CoinMarketCap api

---
+++ cmcapi/coinmarketcap.py
@@ -0,0 +1,88 @@
+import time, requests, json
+import datetime as data
+# from cmcapi.settings import headers, latest_quotes_url
+# ModuleNotFoundError: No module named 'cmcapi'?
+# ModuleNotFoundError: No module named 'bot'?
+
+X_CMC_PRO_API_KEY = "7d82ac5a-fd68-44fd-81fc-23e1637b0f31"
+latest_quotes_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
+headers = {
+ 'Accept': 'application/json',
+ 'Accept-Encoding': 'deflate, gzip',
+ 'X-CMC_PRO_API_KEY': X_CMC_PRO_API_KEY,
+ }
+
+url = latest_quotes_url
+headers = headers
+
+def check_is_symbol(name):
+    is_symbol = False
+    if name.isupper():
+        is_symbol = True
+    return is_symbol
+
+def req(url):
+    request = requests.get(url, headers=headers)
+    data = request.json()
+    if request.status_code == 200:
+        return data
+    else:
+        # todo: handle errors
+        print(data['status']['error_message'])
+
+def form_url(names, convert):
+    fullurl = url
+    count_of_symbols = 0
+    for x in names:
+        if check_is_symbol(x) == True:
+            count_of_symbols += 1
+
+    if count_of_symbols == len(names):
+        fullurl += "symbol="
+    else:
+        fullurl += "slug="
+
+    for x in names:
+        fullurl += str(x) + ","
+    fullurl = fullurl[:-1]
+
+    fullurl += "&convert=" + str(convert)
+
+    return fullurl
+
+
+def get_price(names, convert="USD"):
+    '''
+    names: list of cryptocurrency slugs or symbols (list of strings)
+    convert: symbol of the cryptocurrency or fiat currency we want to convert to (string)
+    usage:
+        get_price(["BTC", "ETH"], "UAH")
+        get_price(["bitcoin", "ethereum"])
+    returns list of dicts
+    '''
+    currencies = {}
+    result = []
+    full_url = form_url(names, convert)
+    data = req(full_url)
+
+    for x in data['data']:
+        currencies[x] = data['data'][x]['name']
+
+    for x in currencies:
+        result.append({
+            "Name" : currencies[x],
+            "Symbol" : data['data'][x]['symbol'],
+            "Rank" : data['data'][x]['cmc_rank'],
+            "Convert currency" : convert,
+            "Current price" : data['data'][x]['quote'][convert]['price'],
+            "Percent change per 1h" : data['data'][x]['quote'][convert]['percent_change_1h'],
+            "Percent change per 24h" : data['data'][x]['quote'][convert]['percent_change_24h'],
+            "Percent change per 7d" : data['data'][x]['quote'][convert]['percent_change_7d'],
+            "Market capacity" : data['data'][x]['quote'][convert]['market_cap']
+        })
+    return result
+
+for x in get_price(["BTC", "ETH"], "UAH"):
+    for y in x:
+        print("{} : {}".format(y, x[y]))
+    print()
---
+++ cmcapi/settings.py
@@ -0,0 +1,10 @@
+# from bot.local_settings import X_CMC_PRO_API_KEY
+X_CMC_PRO_API_KEY = "7d82ac5a-fd68-44fd-81fc-23e1637b0f31"
+
+latest_quotes_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
+
+headers = {
+ 'Accept': 'application/json',
+ 'Accept-Encoding': 'deflate, gzip',
+ 'X-CMC_PRO_API_KEY': X_CMC_PRO_API_KEY,
+}

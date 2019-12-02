6caf4073d00895755606f7a2cb44f14a74e7e313
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-12-02 16:01:04 +0200

    bugs fixed

--- cmcapi/coinmarketcap.py
+++ cmcapi/coinmarketcap.py
@@ -1,16 +1,6 @@
 import time, requests, json
 import datetime as data
 from cmcapi.settings import headers, latest_quotes_url
-# ModuleNotFoundError: No module named 'cmcapi'?
-# ModuleNotFoundError: No module named 'bot'?
-
-# X_CMC_PRO_API_KEY = "7d82ac5a-fd68-44fd-81fc-23e1637b0f31"
-# latest_quotes_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
-# headers = {
-#  'Accept': 'application/json',
-#  'Accept-Encoding': 'deflate, gzip',
-#  'X-CMC_PRO_API_KEY': X_CMC_PRO_API_KEY,
-#  }
 
 url = latest_quotes_url
 headers = headers
--- cmcapi/settings.py
+++ cmcapi/settings.py
@@ -1,5 +1,4 @@
-# from bot.local_settings import X_CMC_PRO_API_KEY
-X_CMC_PRO_API_KEY = "7d82ac5a-fd68-44fd-81fc-23e1637b0f31"
+from bot.local_settings import X_CMC_PRO_API_KEY
 
 latest_quotes_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?'
 
